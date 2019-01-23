9793193fb3985c960b59c545c07ce586970c7d44
carlosclabsis <carlosclabsis@gmail.com>
2019-01-23 15:37:20 -0500

    Generado base de datos Identity
    - Se configuro en el archivo Startup para poder usar este servicio.

---
+++ ExWebApiAutos/ExWebApiAutos/Model/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExWebApiAutos.Model
+{
+    public class AppIdentityDbContext
+    {
+    }
+}
