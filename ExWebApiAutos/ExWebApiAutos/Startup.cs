b9a4963b0e4de9f7cd954d6c89c4b2c2c4c2f5ae
carlosclabsis <carlosclabsis@gmail.com>
2019-01-22 23:09:12 -0500

    Configuracion de Swagger
    Se instalo Swashbuckle.AspNetCore, el cual usaremos como cliente para nuestros servicios Rest.

--- ExWebApiAutos/ExWebApiAutos/Startup.cs
+++ ExWebApiAutos/ExWebApiAutos/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ExWebApiAutos
 {
@@ -25,7 +26,11 @@ namespace ExWebApiAutos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c => 
+            { c.SwaggerDoc("v1", new Info { Title = "ExWebApiAutosServices", Version = "v1" });
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,10 +40,9 @@ namespace ExWebApiAutos
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
+            app.UseSwagger(); app.UseSwaggerUI(c => 
+            { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
